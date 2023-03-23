3497819717d750095c0d42e061decfc1856924d6
Steve Coyne <coyne99@gmail.com>
2023-03-22 20:43:39 -0400

    reapplied template - fixed menu

--- assets/js/module.bootstrap.js
+++
@@ -1,1 +0,0 @@
-export default function(o){const t=SEMICOLON.Core;t.loadJS({file:"assets/js/plugins.bootstrap.js",id:"canvas-bootstrap-js"}),t.isFuncTrue(()=>"undefined"!=typeof bootstrap).then(o=>!!o&&void SEMICOLON.Core.initFunction({class:"has-plugin-bootstrap",event:"pluginBootstrapReady"}))}
